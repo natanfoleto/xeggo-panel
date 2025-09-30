e8ef9f43e230cd7625c9fcb86a691a63cc84b21d
Natan Foleto <natanfoleto2015@hotmail.com>
2025-09-30 18:22:47 -0300

    Rebrand application from pizza.shop to xeggo and update dependencies                                                      │ │ │ │                                                                                                                           │ │ │ │ - Update project name from 'xeggo' to 'xeggo-panel' in package.json                                                       │ │ │ │ - Rebrand HTML title from 'pizza.shop' to 'xeggo'                                                                         │ │ │ │ - Reformat index.html indentation for consistency                                                                         │ │ │ │ - Update @types/react from 18.2.15 to 18.2.37                                                                             │ │ │ │ - Update @types/react-dom from 18.2.7 to 18.2.15                                                                          │ │ │ │ - Update typescript from 5.0.2 to 5.2.2                                                                                   │ │ │ │ - Update vite from 4.4.5 to 4.5.0                                                                                         │ │ │ │ - Update pnpm-lock.yaml with new dependency tree

--- src/app.tsx
+++ src/app.tsx
@@ -12,7 +12,7 @@ import { router } from './routes'
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
+      <Helmet titleTemplate="%s | xeggo" />
       <ThemeProvider defaultTheme="light" storageKey="ifood-theme">
         <QueryClientProvider client={queryClient}>
           <RouterProvider router={router} />
