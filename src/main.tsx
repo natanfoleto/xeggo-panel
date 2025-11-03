2087f1e0f6c13b040a247d9a5d8cfee1db1e7b96
Natan Foleto <natanfoleto2015@hotmail.com>
2025-11-03 14:19:21 -0300

    fix: removed enableMSW

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { enableMSW } from './api/mocks'
 import { App } from './app'
 
-enableMSW().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  )
-})
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
