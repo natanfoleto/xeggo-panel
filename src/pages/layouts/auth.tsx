120a34705e812dffe1bfd8a64f4851674e26c777
Natan Foleto <natanfoleto2015@hotmail.com>
2025-11-15 04:10:03 -0300

    fix: public images

--- src/pages/layouts/auth.tsx
+++ src/pages/layouts/auth.tsx
@@ -7,13 +7,15 @@ export function AuthLayout() {
     <div className="relative min-h-screen flex-col items-center antialiased md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-linear-to-r from-violet-800 to-violet-600 p-10 lg:flex">
         <div className="text-foreground flex items-center gap-3 text-lg font-medium">
-          <span className="font-semibold text-zinc-100">
-            <BrandIcon className="size-6 fill-white" />
+          <span className="rounded-md bg-zinc-200 p-1.5">
+            <BrandIcon className="size-6 fill-violet-500" />
           </span>
         </div>
 
-        <div className="flex flex-1 flex-col items-center justify-center gap-3 text-zinc-100">
-          <BrandIcon className="size-20 fill-white" />
+        <div className="flex flex-1 flex-col items-center justify-center gap-5 text-zinc-100">
+          <span className="rounded-xl bg-zinc-200 p-4">
+            <BrandIcon className="size-14 fill-violet-500" />
+          </span>
 
           <div className="text-center">
             <h1 className="text-3xl">
@@ -26,7 +28,7 @@ export function AuthLayout() {
 
         <div className="mt-auto">
           <footer className="text-sm text-zinc-100">
-            Painel do parceiro &copy; xeggo - {new Date().getFullYear()}
+            Painel do parceiro &copy; Xeggo - {new Date().getFullYear()}
           </footer>
         </div>
       </div>
