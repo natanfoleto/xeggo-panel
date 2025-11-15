e43df9d438884d8295692c47777d90713cced634
Natan Foleto <natanfoleto2015@hotmail.com>
2025-11-15 00:10:33 -0300

    feat: add trial alert

--- src/components/trial-alert.tsx
+++ src/components/trial-alert.tsx
@@ -1,12 +1,51 @@
+import { useQuery } from '@tanstack/react-query'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { getSubscription } from '@/api/manager/subscriptions/get-subscription'
+
 export function TrialAlert() {
+  const navigate = useNavigate()
+
+  const [daysLeft, setDaysLeft] = useState<number>(0)
+
+  const { data: subscription } = useQuery({
+    queryKey: ['subscription'],
+    queryFn: getSubscription,
+  })
+
+  useEffect(() => {
+    if (!subscription?.trialEnd) return
+
+    const trialEndDate = new Date(subscription.trialEnd)
+
+    const calcDays = () => {
+      const diff = trialEndDate.getTime() - Date.now()
+      const days = Math.ceil(diff / (1000 * 60 * 60 * 24))
+      setDaysLeft(days)
+    }
+
+    calcDays()
+    const interval = setInterval(calcDays, 1000 * 60 * 60)
+    return () => clearInterval(interval)
+  }, [subscription])
+
+  if (!subscription?.trialEnd) return null
+
   return (
-    <a href="/upgrade" className="bg-muted px-4 py-2 text-center">
-      <p className="text-sm">
-        Você está usando a versão trial.{' '}
-        <b className="font-semibold text-violet-500 underline">
-          Faça o upgrade agora!
-        </b>
-      </p>
-    </a>
+    <div className="bg-linear-to-r from-violet-500 to-violet-600 px-4 py-1.5 text-center text-white">
+      <div className="text-sm font-medium">
+        <span>Período de teste ativo • </span>
+        <span>
+          Expira em <b>{daysLeft}</b> {daysLeft === 1 ? 'dia' : 'dias'} •{' '}
+        </span>
+        <span
+          className="cursor-pointer underline"
+          onClick={() => navigate('/upgrade')}
+        >
+          Fazer upgrade
+        </span>
+      </div>
+    </div>
   )
 }
